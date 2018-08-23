bd8016c892aaf8358582a399ee489eab21b531c2
Aizar <aizar.cabrera@icloud.com>
2018-08-22 22:03:09 -0600

    Final commit

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,20 +4,67 @@ require 'pry'
 class Deck
 
 attr_reader :count,
-            :cards
+            :cards,
+            :value,
+            :suit
 
   def initialize(cards)
     @count = cards.count
     @cards = cards
+    @value = value
+    @suit  = suit
   end
-# binding.pry
 
 
-#
-#    def sort
-#    value_rank {"2"=>1, "3"=>5, "4"=>9}
-#
-#    suit_rank {"Clubs"=>0, "Diamonds"=>1, "Hearts"=>2, "Spades"=>3 }
-#    end
-#
+  def card_value
+  cards_hash=
+        {
+        "2"      => 1,
+         "3"     => 5,
+         "4"     => 9,
+         "5"     => 13,
+         "6"     => 17,
+         "7"     => 21,
+         "8"     => 25,
+         "9"     => 29,
+         "10"    => 33,
+         "Jack"  => 37,
+         "Queen" => 41,
+         "King"  => 45,
+         "Ace"   => 49,
+        }
+
+
+  suit_hash=
+        {
+        "Clubs"   => 10,
+        "Diamonds"=> 20,
+        "Hearts"  => 30,
+        "Spades"  => 40
+        }
+
+
+  card_value = cards_hash[@value] + suit_hash[@suit]
+ end
+
+
+ def sort
+     card_array = cards
+
+     loop do
+       swap = false
+
+       (card_array.length - 1).times do |card|
+       if card_array[card].card_value > card_array[card + 1].card_value
+         card_array[card], card_array[card + 1] = card_array[card + 1], card_array[card]
+
+         swap = true
+       end
+     end
+
+     break if not swap
+
+    return card_array
+  end
+ end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -11,7 +11,7 @@ class DeckTest< Minitest::Test
    card_1 = Card.new("3", "Hearts")
    card_2 = Card.new("4", "Clubs")
    card_3 = Card.new("5","Diamonds")
-   deck = Deck.new([card_1, card_2, card_3])
+   deck   = Deck.new([card_1, card_2, card_3])
    assert_instance_of Deck, deck
   end
 
@@ -19,7 +19,7 @@ class DeckTest< Minitest::Test
    card_1 = Card.new("3", "Hearts")
    card_2 = Card.new("4", "Clubs")
    card_3 = Card.new("5","Diamonds")
-   deck = Deck.new([card_1, card_2, card_3])
+   deck   = Deck.new([card_1, card_2, card_3])
    assert_equal [card_1, card_2, card_3], deck.cards
   end
 
@@ -27,18 +27,20 @@ class DeckTest< Minitest::Test
    card_1 = Card.new("3", "Hearts")
    card_2 = Card.new("4", "Clubs")
    card_3 = Card.new("5","Diamonds")
-   deck = Deck.new([card_1, card_2, card_3])
+   deck   = Deck.new([card_1, card_2, card_3])
    assert_equal 3, deck.count
 
   end
 
 
   def test_it_sort
-   card_1 = Card.new("3", "Hearts")
-   card_2 = Card.new("4", "Clubs")
-   card_3 = Card.new("5","Diamonds")
-   deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
-   assert_equal [card_1, card_3, card_2, card_5, card_4], deck.sort
+    card_1 = Card.new("4","Hearts")
+    card_2 = Card.new("Jack", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    card_4 = Card.new("Ace", "Spades")
+    card_5 = Card.new("Ace", "Diamonds")
+    deck   = Deck.new([card_1, card_2, card_3, card_4, card_5])
+   assert_equal [card_1, card_3, card_2, card_5, card_4],deck.sort
 
   end
 
